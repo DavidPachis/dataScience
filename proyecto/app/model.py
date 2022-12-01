927cb26a8bfe0fff0483fcefda50e42f61f0515c
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-11-30 22:21:30 -0500

    Update model.py

--- proyecto/app/model.py
+++ proyecto/app/model.py
@@ -3,8 +3,10 @@ import joblib
 
 
 # load model
-url2 = 'https://github.com/DavidPachis/dataScience/raw/main/proyecto/models/model_Cap.pkl'
-model = wget.download(url2)
+URL = 'https://github.com/DavidPachis/dataScience/raw/main/proyecto/models/model_Cap.pkl'
+response = requests.get(URL)
+open("model_Cap.pkl", "wb").write(response.content)
+best_xgboost_model = joblib.load('model_Cap.pkl')
 own_model = joblib.load('model_Cap.pkl')
 best_xgboost_model= own_model
 best_xgboost_model.save_model("best_model.json")
