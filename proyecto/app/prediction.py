c89bdd272fffb2b6afc491d766cf6beefc811ba8
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-11-30 22:22:51 -0500

    Update prediction.py

--- proyecto/app/prediction.py
+++ proyecto/app/prediction.py
@@ -5,9 +5,9 @@ import joblib
 
 
 data = pd.read_csv("https://raw.githubusercontent.com/DavidPachis/dataScience/main/proyecto/data/nueva_entrada.csv")
-# load model
-url2 = 'https://github.com/DavidPachis/dataScience/raw/main/proyecto/models/model_Cap.pkl'
-model = wget.download(url2)
+URL = 'https://github.com/DavidPachis/dataScience/raw/main/proyecto/models/model_Cap.pkl'
+response = requests.get(URL)
+open("model_Cap.pkl", "wb").write(response.content)
 own_model = joblib.load('model_Cap.pkl')
 best_xgboost_model = own_model
 
