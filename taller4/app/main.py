c2f880111dc739ac8b4fe56186f33b5587d108ed
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-12-06 21:26:58 -0500

    Add files via upload

--- taller4/app/main.py
+++ taller4/app/main.py
@@ -27,12 +27,20 @@ st.title('Taller 4, churn Rate')
 
 def load_data():
     uploaded_file = st.file_uploader(label='upload dataset for training')
-    data_train = pd.read_json(
-        'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Entrenamiento_v2.json')
+    #data_train = pd.read_json(
+     #   'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Entrenamiento_v2.json')
     if uploaded_file is not None:
         data_train = uploaded_file.getvalue()
     return data_train
 
+
+def load_pred():
+    uploaded_file = st.file_uploader(label='upload dataset for training')
+    #data_pred = pd.read_json('https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data'
+                 #'/DataSet_Prediccion.json')
+    if uploaded_file is not None:
+        data_pred = uploaded_file.getvalue()
+    return data_pred
 
 def cleaning(dataset):
     # se limpia la columna TotalChares que tiene problemas con valores 0
@@ -133,6 +141,7 @@ def reentrenamiento(df1, df2):
 
 
 if st.checkbox('check for use first model'):
+    load_data()
     # load model
     url = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
     response = requests.get(url)
@@ -140,9 +149,10 @@ if st.checkbox('check for use first model'):
     best_model = joblib.load("my_model.pkl")
 
 if st.button('Make Prediction'):
-    prediction = get_final_pred_mv0(data_pre, best_model)
+    data_predi= load_pred()
+    prediction = get_final_pred_mv0(data_predi, best_model)
     # print("final prediction", np.squeeze(prediction))
-    final_d = pd.DataFrame.to_string(prediction)
+    final_d = st.table(prediction)
     st.write(f"Your churn: {final_d}")
 
 if st.button('Make Prediction with new model'):
