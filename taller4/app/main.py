438c7fc68a76db412d796f44439f1e1a75e1b0b8
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-12-06 19:43:12 -0500

    Add files via upload

--- taller4/app/main.py
+++ taller4/app/main.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-import xgboost as xgb
 import numpy as np
-import wget
+import requests
 import joblib
-
 st.header("Taller 4, churn Rate")
 data_train = pd.read_json(
     'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Entrenamiento_v2.json')
@@ -18,20 +14,20 @@ def load_data():
     if uploaded_file is not None:
         data = uploaded_file.getvalue()
         st.image(data)
-    else:
-        data = pd.read_json(
-            'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Prediccion.json')
 
 
 if st.checkbox('check for use first model'):
     # load model
-    url2 = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
+    url = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
     path = '/content/sample_data'
-    model = wget.download(url2, out=path)
-    best_model = joblib.load("/content/model_Cap.pkl")
+    response = requests.get(url)
+    open("my_model.pkl", "wb").write(response.content)
+    best_model = joblib.load("/content/my_model.pkl")
 
 if st.button('Make Prediction'):
     inputs = data_pre
     prediction = best_model.predict(inputs)
-    print("final", np.squeeze(prediction, -1))
+    print("final prediction", np.squeeze(prediction, -1))
+    final_d=np.array2string(prediction)
+    st.write(f"Your fares: {final_d}g")
 
