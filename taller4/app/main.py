cc4e95e91227e87a4c3b6dfc4eed601dcf1aea98
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-12-06 19:57:21 -0500

    Add files via upload

--- taller4/app/main.py
+++ taller4/app/main.py
@@ -1,33 +1,29 @@
 import streamlit as st
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
 import numpy as np
-import requests
+import wget
 import joblib
-st.header("Taller 4, churn Rate")
-data_train = pd.read_json(
-    'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Entrenamiento_v2.json')
-data_pre = 'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Prediccion.json'
 
+st.header("Fish Weight Prediction App")
+st.text_input("Enter your Name: ", key="name")
+data = pd.read_csv("https://raw.githubusercontent.com/DavidPachis/dataScience/main/proyecto/data/nueva_entrada.csv")
 
-def load_data():
-    uploaded_file = st.file_uploader(label='upload dataset for training')
-    if uploaded_file is not None:
-        data = uploaded_file.getvalue()
-        st.image(data)
+# load model
+url2 = 'https://github.com/DavidPachis/dataScience/raw/main/proyecto/models/model_Cap.pkl'
+path = '/content/sample_data'
+model = wget.download(url2, out=path)
+best_xgboost_model = joblib.load("/content/model_Cap.pkl")
 
-
-if st.checkbox('check for use first model'):
-    # load model
-    url = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
-    path = '/content/sample_data'
-    response = requests.get(url)
-    open("my_model.pkl", "wb").write(response.content)
-    best_model = joblib.load("/content/my_model.pkl")
+if st.checkbox('Show Training Dataframe'):
+    data
 
 if st.button('Make Prediction'):
-    inputs = data_pre
-    prediction = best_model.predict(inputs)
-    print("final prediction", np.squeeze(prediction, -1))
-    final_d=np.array2string(prediction)
-    st.write(f"Your fares: {final_d}g")
+    inputs = data
+    prediction = best_xgboost_model.predict(inputs)
+    print("final pred", np.squeeze(prediction, -1))
+    st.write(f"Your fare: {np.squeeze(prediction, -1):.2f}g")
 
+    st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
