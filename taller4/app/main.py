7c8accd800e6628fd52d30b3557437ffc8fc5353
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-12-06 20:04:15 -0500

    Add files via upload

--- taller4/app/main.py
+++ taller4/app/main.py
@@ -6,23 +6,23 @@ import joblib
 
 data_train = pd.read_json(
     'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Entrenamiento_v2.json')
-data_pre = 'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Prediccion.json'
+data_pre = pd.read_json('https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data'
+                        '/DataSet_Prediccion.json')
 
 
 def load_data():
     uploaded_file = st.file_uploader(label='upload dataset for training')
     if uploaded_file is not None:
         data = uploaded_file.getvalue()
-        st.image(data)
+        st.write(data)
 
 
 if st.checkbox('check for use first model'):
     # load model
     url = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
-    path = '/content/sample_data'
     response = requests.get(url)
     open("my_model.pkl", "wb").write(response.content)
-    best_model = joblib.load("/content/my_model.pkl")
+    best_model = joblib.load("my_model.pkl")
 
 if st.button('Make Prediction'):
     inputs = data_pre
