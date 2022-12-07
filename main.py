d9425693deb47af682c4181925f4bcb6b24dfd9b
David Vasquez Pachon <32931119+DavidPachis@users.noreply.github.com>
2022-12-06 22:55:26 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -24,14 +24,10 @@ data_train_1 = pd.read_json(
 
 st.title('Taller 4, churn Rate')
 
-
-def load_data():
-    uploaded_file = st.file_uploader(label='upload dataset for training')
-    # data_train = pd.read_json(
-    #   'https://raw.githubusercontent.com/DavidPachis/dataScience/main/taller4/data/DataSet_Entrenamiento_v2.json')
-    if uploaded_file is not None:
-        data_train = uploaded_file.getvalue()
-        return data_train
+url = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
+response = requests.get(url)
+open("my_model.pkl", "wb").write(response.content)
+best_model = joblib.load("my_model.pkl")
 
 
 def load_pred():
@@ -143,24 +139,55 @@ def reentrenamiento(df1, df2):
 
 if st.checkbox('check for use first model'):
     # load model
-    url = 'https://github.com/DavidPachis/dataScience/raw/main/taller4/model/my_model.pkl'
-    response = requests.get(url)
-    open("my_model.pkl", "wb").write(response.content)
-    best_model = joblib.load("my_model.pkl")
-    if st.button('Make Prediction'):
-        uploaded_file = st.file_uploader(label='upload dataset for prediction')
-        if uploaded_file is not None:
-            data_predi = uploaded_file.getvalue()
-            prediction = get_final_pred_mv0(data_predi, best_model)
-            print("final prediction", prediction)
-            final_d = prediction
-            st.write(f"Your churn results: {final_d}")
+    uploaded_file = st.file_uploader(label='upload dataset for prediction')
+    final_d = ''
+    if uploaded_file is not None:
+        data_predi = pd.read_json(uploaded_file.getvalue())
+        prediction = get_final_pred_mv0(data_predi, best_model)
+        # print("final prediction", prediction)
+        final_d = st.dataframe(prediction)
+        # st.write(f"Your churn results: {final_d}")
 
-if st.button('Make Prediction with new model'):
-    data_2_train = load_data()
-    # data_2_train = cleaning(data_2_train)
-    model_2 = reentrenamiento(data_2_train, data_train_1)
-    prediction2 = get_final_pred_mv0(data_pre, model_2)
-    # print("final prediction", np.squeeze(prediction2, -1))
-    final_d2 = pd.DataFrame.to_string(prediction2)
-    st.write(f"Your churns: {final_d2}")
+if st.checkbox('check for use second model'):
+    uploaded_file = st.file_uploader(label='upload dataset for training')
+    final_d = ''
+    if uploaded_file is not None:
+        data_2_train = pd.read_json(uploaded_file.getvalue())
+        model_2 = reentrenamiento(data_2_train, data_train_1)
+        uploaded_file = st.file_uploader(label='upload dataset for predictions')
+        if uploaded_file is not None:
+            pred = pd.read_json(uploaded_file.getvalue())
+            prediction2 = get_final_pred_mv0(pred, model_2)
+            print("final prediction", prediction2)
+            final_d = st.dataframe(prediction2)
+            # metrics
+            clean_df2 = cleaning(data_2_train)
+            clean_df1 = cleaning(data_train_1)
+            rtrain_df = pd.concat([clean_df1, clean_df2], axis=0)
+
+            X_last = rtrain_df.drop("Churn", axis=1)
+            Y_last = rtrain_df["Churn"]
+            X_train, X_test, Y_train, Y_test = train_test_split(X_last, Y_last, test_size=0.3, stratify=Y_last,
+                                                                random_state=33)
+            predictions_v1 = best_model.predict(X_test)
+
+            print(classification_report(Y_test, predictions_v1))
+            a = classification_report(Y_test, predictions_v1)
+            df_a = pd.DataFrame(a).transpose()
+            st.dataframe(df_a)
+            print(roc_auc_score(Y_test, best_model.predict_proba(X_test)[:, 1]))
+            b = roc_auc_score(Y_test, best_model.predict_proba(X_test)[:, 1])
+            st.write(f"Your original roc_auc_score: {b}")
+
+            # segundo modelo
+
+            predictions_v2 = model_2.predict(X_test)
+
+            print(classification_report(Y_test, predictions_v2))
+            c = classification_report(Y_test, predictions_v2)
+            df_c = pd.DataFrame(c).transpose()
+            st.dataframe(df_c)
+            print(roc_auc_score(Y_test, model_2.predict_proba(X_test)[:, 1]))
+            d = roc_auc_score(Y_test, model_2.predict_proba(X_test)[:, 1])
+            # df_d = pd.DataFrame(d).transpose()
+            st.write(f"Your new roc_auc_score: {d}")
